9d8a41f35734634f89b2904d0fc1085c1b02bf04
Josue Ramon Zavala Soto <josuez-c@herbalife.com>
2019-10-03 18:14:15 -0500

    Adding styles warehouses and skus restrictions

--- src/components/PaymentRestrictions.js
+++ src/components/PaymentRestrictions.js
@@ -34,7 +34,7 @@ class PaymentRestrictions extends React.Component{
                 />
                 <div className="row">
                  <div className={"first-step " + (this.state.display ? '' : 'hideContainer')}>
-                    <div className="restrictions-container">
+                    <div className="locales-container">
                       <div className="row">
                         <div className="title-segment">
                           <label> Locales: </label>
@@ -71,27 +71,44 @@ class PaymentRestrictions extends React.Component{
                         </div>
                       </div>
                       <div className="row">
-                        <div className="options-container">
-                          <div className="nowrap-row">
-                            <OptionCard text="es-AR" inputName="locales"/>
-                            <OptionCard text="es-MX" inputName="locales"/>
-                            <OptionCard text="es-PE" inputName="locales"/>
-                            <OptionCard text="en-US" inputName="locales"/>
-                            <OptionCard text="en-PH" inputName="locales"/>
-                            <OptionCard text="en-MX" inputName="locales"/>
-                            <OptionCard text="en-CA" inputName="locales"/>
-                            <OptionCard text="fr-CA" inputName="locales"/>
-                            <OptionCard text="fr-FR" inputName="locales"/>
-                          </div>
+                        <div className="col-6 options-container general-restrictions">
+
+                        </div>
+                        <div className="col-5 options-container general-restrictions">
+
                         </div>
                       </div>
                     </div>
-                    <div className="actions-container">
-                      <button className="action-button">Next</button>
+                    <div className="actions-restrictions-container">
+                      <button className="action-button">Save</button>
+                      <button className="action-button">Reset</button>
                     </div>
                  </div>
                 </div>
 
+                <div className="row">
+                 <div className={"third-step " + (this.state.display ? '' : 'hideContainer')}>
+                    <div className="restrictions-container">
+                      <div className="row">
+                        <div className="title-segment">
+                          <label> SKU's Restrictions: </label>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-xl-5 col-lg-5 col-md-5 col-sm-5 options-container general-restrictions">
+
+                        </div>
+                        <div className="col-xl-5 col-lg-4 col-md-4 col-sm-4 options-container general-restrictions">
+
+                        </div>
+                      </div>
+                    </div>
+                    <div className="actions-restrictions-container">
+                      <button className="action-button">Save</button>
+                      <button className="action-button">Reset</button>
+                    </div>
+                 </div>
+                </div>
               </div>
             </div>
         </div>
