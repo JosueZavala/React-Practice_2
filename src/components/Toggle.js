7d2c34addc71acd21ae23ed2f0b618ff18aeb603
Josue Ramon Zavala Soto <josuez-c@herbalife.com>
2019-10-07 17:27:19 -0500

    Moving showAlert (function when toggle changes), from Toggle.js to WHTuple.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import '../Styles/Toggle.css';
-import SweetAlert from 'sweetalert2-react';
 
 class Toggle extends React.Component {
 
     constructor ( props ) {
         super( props );
         this._handleChange = this._handleChange.bind(this);
-        this.SetSweetAlert = this.SetSweetAlert.bind(this);
 
     		this.state = {
-    			isChecked: this.props.isChecked,
-          nameToShow: this.props.warehouseName,
-          show: false,
-          swaltype: "",
-          swaltitle: "",
-          swaltext: "",
+    			isChecked: this.props.isChecked
     		}
-
-    }
-
-    SetSweetAlert(type, title, text) {
-        this.setState({
-          show: true,
-          swaltype: type,
-          swaltitle: title,
-          swaltext: text
-        });
     }
 
     render () {
@@ -37,36 +20,12 @@ class Toggle extends React.Component {
               <input type="checkbox" checked={ this.state.isChecked } onChange={ this._handleChange }></input>
               <span className="slider round"></span>
             </label>
-            <SweetAlert
-              show={this.state.show}
-              type={this.state.swaltype}
-              title={this.state.swaltitle}
-              text={this.state.swaltext}
-              onConfirm={() => this.setState({ show: false })}
-            />
           </div>
         );
     }
 
-
     _handleChange () {
-      let isChecked = this.state.isChecked;
-      let sweetAlertTitle = this.state.nameToShow;
-      const updateIsIkiosk = new Promise((resolve, reject) => {
-          setTimeout(() => !isChecked ? resolve(isChecked) : reject(isChecked)
-            , 300);
-      });
-
-      updateIsIkiosk
-        .then((value) => {
-          this.setState({ isChecked: !this.state.isChecked});
-          this.SetSweetAlert("success", sweetAlertTitle, "Ikiosk changed to True")
-        })
-        .catch((error) => {
-          this.setState({ isChecked: !this.state.isChecked});
-          console.error(error);
-          this.SetSweetAlert("error",sweetAlertTitle,"Ikiosk changed to False")
-        });
+      this.setState({ isChecked: !this.state.isChecked});
       this.props.toggleChanged();
     }
 
--- src/components/WHTuple.js
+++ src/components/WHTuple.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MyModal  from '../components/MyModal';
 import Toggle from '../components/Toggle';
+import SweetAlert from 'sweetalert2-react';
 
 class WHTuple extends React.Component{
   constructor(props){
     super(props);
 
     this.onToggle = this.onToggle.bind(this);
+    this.showAlert = this.showAlert.bind(this);
 
     this.state = {
       show : false,
@@ -14,20 +16,57 @@ class WHTuple extends React.Component{
       handleClose: () => this.setState({ show: false }),
       toggleActive: false,
       isIkiosk: this.props.dataObject.isIkiosk,
-      dataObject: this.props.dataObject
+      dataObject: this.props.dataObject,
+      showSA : false,
+      swaltype: "",
+      swaltitle: "",
+      swaltext: "",
     }
-  }
 
-  onToggle() {
+  }
+  onToggle () {
     let newObject = this.state.dataObject;
     newObject.isIkiosk = !this.state.isIkiosk;
 
-   this.setState({
-     toggleActive: !this.state.toggleActive,
-     isIkiosk: !this.state.isIkiosk,
-     dataObject: newObject
-   });
+     this.setState({
+       toggleActive: !this.state.toggleActive,
+       isIkiosk: !this.state.isIkiosk,
+       dataObject: newObject
+     });
+
+     this.showAlert();
   }
+
+  showAlert(){
+      let isChecked = this.state.isIkiosk;
+      let sweetAlertTitle = this.state.dataObject.Name;
+      const updateIsIkiosk = new Promise((resolve, reject) => {
+          setTimeout(() => !isChecked ? resolve(isChecked) : reject(isChecked)
+            , 300);
+      });
+
+      updateIsIkiosk
+        .then((value) => {
+          this.SetSweetAlert("success", sweetAlertTitle, "Ikiosk changed to True")
+        })
+        .catch((error) => {
+          console.error(error);
+          this.SetSweetAlert("error",sweetAlertTitle,"Ikiosk changed to False")
+        });
+    }
+
+    SetSweetAlert(type, title, text) {
+        this.setState({
+          showSA: true,
+          swaltype: type,
+          swaltitle: title,
+          swaltext: text
+        });
+    }
+
+
+
+
 
   render() {
 
@@ -38,22 +77,28 @@ class WHTuple extends React.Component{
           <td>{this.state.dataObject.Name}</td>
           <td>{this.state.isIkiosk.toString()}</td>
           <td>
-              <Toggle
-                isChecked = { this.state.isIkiosk }
-                warehouseName = { this.state.dataObject.Name }
-                toggleChanged = { this.onToggle }
-              />
+            <Toggle
+              isChecked = { this.state.isIkiosk }
+              toggleChanged = { this.onToggle }
+            />
           </td>
           <td className="text-center">
             <div className="btn btn-primary btn-sm" onClick={this.state.handleShow}>
               <i className="fa fa-eye"></i>
             </div>
           </td>
-          <MyModal
-            show = {this.state.show}
-            handleClose = {this.state.handleClose}
-            data = {this.state.dataObject}
-          />
+            <MyModal
+              show = {this.state.show}
+              handleClose = {this.state.handleClose}
+              data = {this.state.dataObject}
+            />
+            <SweetAlert
+              show={this.state.showSA}
+              type={this.state.swaltype}
+              title={this.state.swaltitle}
+              text={this.state.swaltext}
+              onConfirm={() => this.setState({ showSA: false })}
+            />
         </tr>
     );
   }
