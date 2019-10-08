9b78723feff2a2ad6e17d503c3d5a1b6d9f4adfc
Josue Ramon Zavala Soto <josuez-c@herbalife.com>
2019-10-07 21:50:39 -0500

    Adding function _handleOnChange, it will invoke the function gived on props.FunctionOnChanged, changed for for htmlFor radio property

--- src/components/OptionCard.js
+++ src/components/OptionCard.js
@@ -2,19 +2,29 @@ import React from 'react';
 import '../Styles/OptionCard.css';
 
 class OptionCard extends React.Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-
+      cardValue: this.props.text
     };
+  }
+
+  _handleOnChange(e){
+    this.props.FunctionOnChange(e.target.value);
   }
 
   render() {
 
     return (
       <div>
-        <label className="principal-card" for={this.props.text}>
-          <input type="radio" name={this.props.inputName} value={this.props.text} id={this.props.text} />
+        <label className="principal-card" htmlFor={this.props.text}>
+          <input
+            type = "radio"
+            name = {this.props.inputName}
+            value = {this.props.text}
+            id = {this.props.text}
+
+            onChange = {evt => this._handleOnChange(evt)}/>
           {this.props.text}
         </label>
       </div>
