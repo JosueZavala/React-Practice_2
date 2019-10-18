d08f1ecd15b15220d10fbaec98f36c3911e214de
Josue Ramon Zavala Soto <josuez-c@herbalife.com>
2019-10-17 21:54:45 -0500

    Adding class table-styles from WHTable.css, and Changing table header Code by Warehouse Code

--- src/components/WHTable.js
+++ src/components/WHTable.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import '../Styles/WHTable.css';
 
 class WHTable extends React.Component{
-  
+
   render() {
     return (
-        <div className="row">
+        <div className="row table-styles">
           <table className="table table-striped">
             <thead>
               <tr>
-                <th>Code</th>
+                <th>Warehouse Code</th>
                 <th>Country Code</th>
                 <th>Name</th>
                 <th>Is Pickup Location?</th>
