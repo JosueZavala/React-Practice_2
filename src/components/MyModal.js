03b3cd6961dc52fb8ae95fbd7ee5b81d4587372b
Josue Ramon Zavala Soto <josuez-c@herbalife.com>
2019-10-17 22:01:44 -0500

    Changing MyModal function by Class, adding input text and checkbox instead of just data to editing by final user and it correspondant states and onChange event, adding save button and some styles

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -3,63 +3,96 @@ import { Modal } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import '../Styles/MyModal.css';
 
-function MyModal(props) {
-  return (
-    <Modal
-      show = {props.show}
-      onHide = {props.handleClose}
-      size = "xl"
-    >
-     <Modal.Header closeButton>
-       <Modal.Title>Warehouse: {props.data.Name}</Modal.Title>
-     </Modal.Header>
-     <Modal.Body>
-       <table className="table table-striped Modalbody">
-         <thead>
-           <tr>
-             <th>Code</th>
-             <th>Name</th>
-             <th>Short Name</th>
-             <th>Latitude</th>
-             <th>Longitude</th>
-             <th>Address 1</th>
-             <th>Address 2</th>
-             <th>City</th>
-             <th>Country District</th>
-             <th>State Province Territory</th>
-             <th>Postal Code</th>
-             <th>Ikiosk</th>
-             <th>Country Code</th>
-             <th>Phone</th>
-           </tr>
-         </thead>
-         <tbody>
-           <tr>
-             <td>{props.data.Code}</td>
-             <td>{props.data.Name}</td>
-             <td>{props.data.ShortName}</td>
-             <td>{props.data.Latitude}</td>
-             <td>{props.data.Longitude}</td>
-             <td>{props.data.Address1}</td>
-             <td>{props.data.Address2}</td>
-             <td>{props.data.City}</td>
-             <td>{props.data.CountryDistrict}</td>
-             <td>{props.data.StateProvinceTerritory}</td>
-             <td>{props.data.PostalCode}</td>
-             <td>{props.data.isIkiosk.toString()}</td>
-             <td>{props.data.CountryCode}</td>
-             <td>{props.data.phone}</td>
-           </tr>
-         </tbody>
-       </table>
-     </Modal.Body>
-     <Modal.Footer>
-       <Button variant="secondary" onClick={props.handleClose}>
-         Close
-       </Button>
-     </Modal.Footer>
-   </Modal>
-  );
-}
+class MyModal extends React.Component{
+  constructor(props){
+    super(props);
+    this.SaveModalValues = this.SaveModalValues.bind(this);
 
-export default MyModal;
+    this.state = {
+      dataObject: {...this.props.data},
+      whCode: ''
+    }
+  }
+
+  handleChange(evt){
+    let eventValue = evt.target.value;
+    const eventName = evt.target.name;
+    const temporalObject = {...this.state.dataObject}
+
+    if(eventName==='isIkiosk'){
+      eventValue=evt.target.checked;
+    }
+
+    temporalObject[eventName] = eventValue;
+    this.setState({dataObject: temporalObject})
+  }
+
+  SaveModalValues(){
+    this.props.handleSave(this.state.dataObject);
+    this.props.handleClose();
+  }
+
+    render(){
+      return (
+        <Modal
+          show = {this.props.show}
+          onHide = {this.props.handleClose}
+          size = "xl"
+        >
+         <Modal.Header closeButton>
+           <Modal.Title>Warehouse: {this.props.data.Name}</Modal.Title>
+         </Modal.Header>
+         <Modal.Body className="Modalbody">
+           <table className="table table-striped">
+             <thead>
+               <tr>
+                 <th>Code</th>
+                 <th>Name</th>
+                 <th>Short Name</th>
+                 <th>Latitude</th>
+                 <th>Longitude</th>
+                 <th>Address 1</th>
+                 <th>Address 2</th>
+                 <th>City</th>
+                 <th>Country District</th>
+                 <th>State Province Territory</th>
+                 <th>Postal Code</th>
+                 <th>Ikiosk</th>
+                 <th>Country Code</th>
+                 <th>Phone</th>
+               </tr>
+             </thead>
+             <tbody>
+               <tr>
+                 <td><input type="text" name="warehouseCode" value={this.state.dataObject.warehouseCode} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="title" value={this.state.dataObject.title} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="shortName" value={this.state.dataObject.shortName} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="latitude" value={this.state.dataObject.latitude} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="longitude" value={this.state.dataObject.longitude} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="line1" value={this.state.dataObject.line1} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="line2" value={this.state.dataObject.line2} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="city" value={this.state.dataObject.city} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="countryDistrict" value={this.state.dataObject.countryDistrict} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="stateProvinceTerritory" value={this.state.dataObject.stateProvinceTerritory} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="postalCode" value={this.state.dataObject.postalCode} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="checkbox" name="isIkiosk" checked={this.state.dataObject.isIkiosk} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="countryCode" value={this.state.dataObject.countryCode} onChange={evt => this.handleChange(evt)}/></td>
+                 <td><input type="text" name="phone" value={this.state.dataObject.phone} onChange={evt => this.handleChange(evt)}/></td>
+               </tr>
+             </tbody>
+           </table>
+         </Modal.Body>
+         <Modal.Footer>
+           <Button variant="success" onClick={this.SaveModalValues}>
+             Save
+           </Button>
+           <Button variant="danger" onClick={this.props.handleClose}>
+             Close
+           </Button>
+         </Modal.Footer>
+       </Modal>
+      );
+    }
+  }
+
+  export default MyModal;
